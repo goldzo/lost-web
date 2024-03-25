4adb6bcab24f9867ccf981baec8e26177800aae4
gold.fy <156623102+goldzo@users.noreply.github.com>
2024-03-25 18:00:12 -0400

    Delete cmds directory

--- cmds/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const home = document.querySelector(".home");
-
-  home.addEventListener("click", function () {
-    window.location.replace("http://lostfr.vercel.app/home/");
-  });
-});
